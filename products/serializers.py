55f3b22d9a1cd684ffa14f0e03cacb67a4e53498
unknown <thalesvrpb21@gmail.com>
2023-05-03 10:01:55 -0300

    create product serializer

--- products/serializers.py
+++ products/serializers.py
@@ -1,1 +1,17 @@
-#teste
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from .models import Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(
+        validators=[UniqueValidator(queryset=Product.objects.all())],
+    )
+
+    class Meta:
+        model = Product
+        fields = ["id", "description", "price", "quantity", "is_available", "category"]
+        read_only_fields = ["id"]
+
+    def create(self, validated_data):
+        return Product.objects.create(**validated_data)
