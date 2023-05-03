4adc89e4a2ac6a00519af949d99ab643286e56e6
lucasbatista-dev <lucas_pivatto@hotmail.com.br>
2023-05-03 13:43:42 -0300

    feat: modify user

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 from .models import User
+from addresses.serializers import AddressSerializer
+from addresses.models import Address
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -11,12 +13,15 @@ class UserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
         validators=[UniqueValidator(queryset=User.objects.all())],
     )
+    address = AddressSerializer(required=True)
 
     def create(self, validated_data: dict) -> User:
+        address_data = validated_data.pop("address")
+        address = Address.objects.create(**address_data)
         if validated_data["is_superuser"]:
-            user = User.objects.create_superuser(**validated_data)
+            user = User.objects.create_superuser(address=address, **validated_data)
         else:
-            user = User.objects.create_user(**validated_data)
+            user = User.objects.create_user(address=address, **validated_data)
         return user
 
     def update(self, instance: User, validated_data: dict) -> User:
@@ -41,5 +46,6 @@ class UserSerializer(serializers.ModelSerializer):
             "last_name",
             "is_superuser",
             "is_seller",
+            "address",
         ]
-        read_only_fields = ['id']
+        read_only_fields = ["id"]
