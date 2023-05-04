6517401a7b306b3d56c1e2fa0c1230f895658953
Murilo517 <murilosena07@hotmail.com>
2023-05-04 09:47:02 -0300

    hotfix cart serializer

--- carts/serializers.py
+++ carts/serializers.py
@@ -10,8 +10,8 @@ class CartSerializer(serializers.ModelSerializer):
         read_only_fields = ["id"]
 
     def create(self, validated_data: dict) -> Cart:
-        product_avaibility = Product.objects.get("is_available")
-        if not product_avaibility:
+        product_availability = Product.objects.get("is_available")
+        if not product_availability:
             raise ValueError("This item is no longer in stock")
 
         return Cart.objects.create(**validated_data)
