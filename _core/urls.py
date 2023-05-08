c8f6948c617aceed3a446af8053342a7b7fad2a7
lucasbatista-dev <lucas_pivatto@hotmail.com.br>
2023-05-08 18:08:55 -0300

    feat: add doc and deploy config

--- _core/urls.py
+++ _core/urls.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("users.urls")),
     path("api/", include("products.urls")),
-    path("api/", include("carts.urls"))    
+    path("api/", include("carts.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/docs/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/docs/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"
+    ),
 ]
